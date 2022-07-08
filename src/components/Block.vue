559300fd5415d853f8c12eb2791cdb139023c25b
Stephen Moore <judo2000@comcast.net>
2022-07-07 19:26:57 -0600

    Create block component and initial isPlaying and delay data variables and start function

---
+++ src/components/Block.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block">Click Me</div>
+</template>
+
+<script>
+export default {
+  props: ['delay'],
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
